1abac060a1ca076b0f947222f04f5456b5f3fd72
molhot <sakata19991214@gmail.com>
2022-12-24 20:29:15 +0900

    [ #6]split is move or not move iby env

--- split.c
+++ split.c
@@ -6,7 +6,7 @@
 /*   By: satushi <sakata19991214@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/24 18:58:28 by kazuki            #+#    #+#             */
-/*   Updated: 2022/12/24 20:01:29 by satushi          ###   ########.fr       */
+/*   Updated: 2022/12/24 20:28:14 by satushi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,18 +65,72 @@ char **split(char *string, char separater)
     return (command_all);
 }
 
-int main()
-{
-    char *command = " cat     etc     or     ";
-    char separator = ' ';
-    char **command_all;
-	int i;
+// int main()
+// {
+//     char *command = " cat     etc     or     ";
+// 	char *command_1 = "h u l l";
+//     char separator = ' ';
+//     char **command_all;
+// 	char **command_all_1;
+// 	int i;
 
-	i = 0;
-    command_all = split(command, separator);
-	while (i != 3)
+// 	i = 0;
+//     command_all = split(command, separator);
+// 	while (i != 3)
+// 	{
+// 		//write(1, command_all[i], strlen(command_all[i]));
+// 		printf("%s", command_all[i]);
+// 		i++;
+// 	}
+// 	command_all_1 = split(command_1, separator);
+// 	i = 0;
+// 	while (i != 4)
+// 	{
+// 		printf("%s", command_all_1[i]);
+// 		i++;
+// 	}
+// }
+
+void	test_split(void)
+{
+	char	*test1;
+	char	**res1;
+	char	*test2;
+	char	**res2;
+	char	*test3;
+	char	**res3;
+	char	*test4;
+	char	**res4;
+
+	test1 = "hoge huga";
+	res1 = split(test1, ' ');
+	for (int i = 0; i < 2; i++)
 	{
-		printf("%s\n", command_all[i]);
-		i++;
+		printf("%s \n", res1[i]);
 	}
+	test2 = " hoge huga"; 
+	res2 = split(test2, ' '); // test2 を受け取れていない .... //satushi受け取れる...？
+	for (int i = 0; i < 2; i++)
+	{
+		printf("%s \n", res2[i]);
+	}
+	test3 = "     h       u         ";
+	res3 = split(test3, ' ');
+	for (int i = 0; i < 2; i++)
+	{
+		printf("%s \n", res3[i]);
+	}
+	test4 = "";
+	res4 = split(test4, ' ');
+	for (int i = 0; i < 0; i++)
+	{
+		printf("%s \n", res4[i]);
+	}
+}
+
+int	main(void)
+{
+	test_split();
+    // test_len();
+	return (0);
 }
