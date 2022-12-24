b60a2bb9ece56bc065b8cb5d2014cfad9ee2e923
ntk221 <nittakdub@gmail.com>
2022-12-24 18:12:12 +0900

    [Update #6] implemented feature that prompt waits input

--- main.c
+++ main.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 int main(void)
 {
-    write(1, "-->", 4);
+    char *command;
+
+    command = (char *)malloc(sizeof(char) * 100);
+    //write(1, "-->", 4);
     while(1)
     {
+        write(STDOUT_FILENO, "-->", 4);
+        read(STDIN_FILENO, command, 100);
     }
     return (0);
 }
