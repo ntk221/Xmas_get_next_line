58e23f78d5f1d9a8071654bf847e63519c78ee62
ntk221 <nittakdub@gmail.com>
2022-12-24 18:18:25 +0900

    [Upload #6] implemented receive command

--- main.c
+++ main.c
@@ -1,17 +1,28 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+
+// void command_separate(char *command)
+// {
+    
+// }
 
 int main(void)
 {
     char *command;
+    int     flag;
 
     command = (char *)malloc(sizeof(char) * 100);
     //write(1, "-->", 4);
     while(1)
     {
         write(STDOUT_FILENO, "-->", 4);
-        read(STDIN_FILENO, command, 100);
+        flag = read(STDIN_FILENO, command, 100);
+        printf("%s\n", command);
+        bzero((void *)command, 100);
+        if (flag == -1)
+            exit (1);
     }
     return (0);
 }
