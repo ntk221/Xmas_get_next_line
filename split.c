b07813acf244b07ece817e7e0b68cf64e9ec1023
molhot <sakata19991214@gmail.com>
2022-12-24 20:02:00 +0900

    [ #6] split is ok?

--- split.c
+++ split.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   split.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kazuki <kazuki@student.42.fr>              +#+  +:+       +#+        */
+/*   By: satushi <sakata19991214@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/24 18:58:28 by kazuki            #+#    #+#             */
-/*   Updated: 2022/12/24 19:28:57 by kazuki           ###   ########.fr       */
+/*   Updated: 2022/12/24 20:01:29 by satushi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-void **split(char *string, char separater)
+char **split(char *string, char separater)
 {
     size_t  position;
-    int command_num;
+    size_t  command_num;
     char    **command_all;
     size_t  each_command_num;
     int     command_position;
@@ -35,39 +35,48 @@ void **split(char *string, char separater)
         position++;
     }
     command_num = ((command_num + 1) / 2) + 1;
-    command_all = (char **)malloc(sizeof(char *) * command_num);
-    return ;
-    // position = 0;
-    // each_command_num = 0;
-    // while (position != command_num)
-    // {
-    //     while (string[position] == ' ')
-    //         position++;
-    //     while (string[position] != ' ')
-    //     {
-    //         position++;
-    //         each_command_num++;
-    //     }
-    //     position = position - each_command_num;
-    //     command_all[command_position] = (char *)malloc(sizeof(char) * (each_command_num + 1));
-    //     while (string[position] != ' ')
-    //     {
-    //         command_all[command_position][i] = string[position];
-    //         position++;
-    //         i++;
-    //     }
-    //     i = 0;
-    //     command_position++;
-    // }
-    // command_all[command_num] = NULL;
-    // return (command_all);
+    command_all = (char **)malloc(sizeof(char *) * (command_num + 1));
+    position = 0;
+    each_command_num = 0;
+	command_position = 0;
+    while (command_position != command_num)
+    {
+        while (string[position] == ' ')
+            position++;
+        while (string[position] != ' ')
+        {
+            position++;
+            each_command_num++;
+        }
+        position = position - each_command_num;
+        command_all[command_position] = (char *)malloc(sizeof(char) * (each_command_num + 1));
+        while (string[position] != ' ')
+        {
+            command_all[command_position][i] = string[position];
+            position++;
+            i++;
+        }
+		command_all[command_position][i] = '\0';
+        i = 0;
+        command_position++;
+		each_command_num = 0;
+    }
+    command_all[command_num] = NULL;
+    return (command_all);
 }
 
 int main()
 {
-    char *command = "cat etc";
+    char *command = " cat     etc     or     ";
     char separator = ' ';
     char **command_all;
+	int i;
 
-    split(command, separator);
+	i = 0;
+    command_all = split(command, separator);
+	while (i != 3)
+	{
+		printf("%s\n", command_all[i]);
+		i++;
+	}
 }
