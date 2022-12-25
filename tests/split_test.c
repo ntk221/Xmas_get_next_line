30685777b56029618a559fa4584424fabc38753a
ntk221 <nittakdub@gmail.com>
2022-12-25 17:44:02 +0900

    [Update #6] fix split_test

--- tests/split_test.c
+++ tests/split_test.c
@@ -3,14 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   split_test.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: satushi <sakata19991214@gmail.com>         +#+  +:+       +#+        */
+/*   By: kazuki <kazuki@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/24 18:42:36 by kazuki            #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2022/12/24 22:27:58 by satushi          ###   ########.fr       */
-=======
-/*   Updated: 2022/12/24 20:25:43 by kazuki           ###   ########.fr       */
->>>>>>> 23f6c3874bd8c784b3dbe876464db8130556dd87
+/*   Updated: 2022/12/25 17:42:09 by kazuki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,25 +14,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include "../include/minishell.h"
-
-/*int     len(char *res[])
-{
-    int len;
-
-    len = 0;
-    while(res != NULL)
-    {
-        res++;
-        len++;
-    }
-    return len;
-}
-
-void    test_len(void)
-{
-    char hoge[3][5] ={"hoge", "huga", "hello"}; 
-    len(hoge);
-}*/
 
 /*void	test_split(void)
 {
@@ -60,23 +37,22 @@ void    test_len(void)
 		write(1, res1[i], strlen(res1[i]));
 	}
 	test2 = " hoge huga"; 
-	res2 = split(test2, ' '); // test2 を受け取れていない ....
+	res2 = split(test2, ' ');
 	for (int i = 0; i < 2; i++)
 	{
 		write(1, res2[i], strlen(res2[i]));
 	}
-	/*test3 = "     h       u         ";
+	test3 = "     h       u         ";
 	res3 = split(test3, ' ');
 	for (int i = 0; i < 2; i++)
 	{
 		printf("%s \n", res3[i]);
-	}*/
-	/*test4 = "";
+	}
+	test4 = "";
 	res4 = split(test4, ' ');
 	for (int i = 0; i < 0; i++)
 	{
 		printf("%s \n", res4[i]);
-<<<<<<< HEAD
 	}
 	test5 = "             ";
 	res5 = split(test5, ' ');
@@ -87,57 +63,8 @@ void    test_len(void)
 	}
 }*/
 
-
-char	**string_to_split(char *string, int command_num)
-{
-	char **command_all = (char **)malloc(sizeof(char *) * (command_num + 1));
-	size_t	position;
-	int 	each_command_num = 0;
-	int 	command_position = 0;
-	int		i;
-
-	while (command_position != command_num)
-    {
-        while (string[position] == ' ')
-            position++;
-        while (string[position] != ' ' && string[position] != '\0')
-        {
-            position++;
-            each_command_num++;
-        }
-        position = position - each_command_num;
-        command_all[command_position] = (char *)malloc(sizeof(char) * (each_command_num + 1));
-		// printf("each command num is %d\n", each_command_num);
-        while (string[position] != ' ')
-        {
-            command_all[command_position][i] = string[position];
-            position++;
-            i++;
-        }
-		position++;
-		command_all[command_position][i] = '\0';
-        i = 0;
-        command_position++;
-		each_command_num = 0;
-    }
-	command_all[command_num] = NULL;
-	return (command_all);
-=======
-	} */
->>>>>>> 23f6c3874bd8c784b3dbe876464db8130556dd87
-}
-
-void	test_string_to_split(void)
-{
-	char *string = "echo 12";
-	char **res = string_to_split(string, 2);
-}
-
-
 int	main(void)
 {
 	// test_split();
-    // test_len();
-	test_string_to_split();
 	return (0);
 }
