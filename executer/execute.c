475d1390754ba9e34a2700dc703406646f55ca70
molhot <86003237+molhot@users.noreply.github.com>
2022-12-28 12:02:22 +0000

    [Update #60] refactoring!

--- executer/execute.c
+++ executer/execute.c
@@ -6,7 +6,7 @@
 /*   By: satushi <sakata19991214@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/25 18:44:06 by satushi           #+#    #+#             */
-/*   Updated: 2022/12/26 11:05:47 by satushi          ###   ########.fr       */
+/*   Updated: 2022/12/28 11:56:11 by satushi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ bool echo_or_not(char *sub)
 	return (true);
 }
 
-void obtain_commandinfo(char **input_string_all, int *inputstringposition, int input_all)//, int separatednum
+void execute(char **input_string_all, int *inputstringposition, int input_all)//, int separatednum
 {
 	if (true == echo_or_not(input_string_all[*inputstringposition]))
 		ft_echo(input_string_all, inputstringposition); // echo の 引数の有無を判定して処理に回す
